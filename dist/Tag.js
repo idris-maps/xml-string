2f6a0e310f193b92eec85279678322f917defe8d
idris-maps <idris-maps@gmx.ch>
2018-11-03 00:26:49 +0100

    Build version 2.0.0

---
+++ dist/Tag.js
@@ -0,0 +1,61 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var stringifyAttributes = function (attributes) {
+    return Object.keys(attributes)
+        .map(function (key) { return " " + key + "=\"" + String(attributes[key]) + "\""; })
+        .join('');
+};
+var Tag = /** @class */ (function () {
+    function Tag(tag) {
+        this.children = [];
+        this.attributes = {};
+        this.innerText = '';
+        this.tag = tag;
+    }
+    Tag.prototype.child = function (tag) {
+        var child = new Tag(tag);
+        this.children.push(child);
+        return child;
+    };
+    Tag.prototype.c = function (tag) {
+        return this.child(tag);
+    };
+    Tag.prototype.attr = function (newAttributes) {
+        var previousAttributes = this.attributes;
+        this.attributes = __assign({}, previousAttributes, newAttributes);
+        return this;
+    };
+    Tag.prototype.a = function (newAttributes) {
+        return this.attr(newAttributes);
+    };
+    Tag.prototype.data = function (innerText) {
+        this.innerText = innerText;
+        return this;
+    };
+    Tag.prototype.d = function (innerText) {
+        return this.data(innerText);
+    };
+    Tag.prototype.outer = function () {
+        return "<" + this.tag + stringifyAttributes(this.attributes) + ">" + this.inner() + "</" + this.tag + ">";
+    };
+    Tag.prototype.inner = function () {
+        return this.children.length > 0
+            ? this.children
+                .map(function (child) { return child.outer(); })
+                .join('')
+            : this.innerText;
+    };
+    return Tag;
+}());
+exports.default = Tag;
---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Tag_1 = require("./Tag");
+exports.default = {
+    create: function (tag) { return new Tag_1.default(tag); }
+};
